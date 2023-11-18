8db825b947a23a1990a56bc98c997e6ae5e16463
IrishLeigh <irishleigh.sanjuan@cit.edu>
2023-11-18 23:05:45 +0800

    added UserAboutUs.js

---
+++ src/User/UserAboutUs.js
@@ -0,0 +1,26 @@
+import React from "react"
+import ResponsiveAppBar from "../../Components/header"
+import EventCatBtn from "../../Components/EventCatBtn"
+import "../EventCategory/eventCategory.css"
+import "../../Components/EventCatBtn.css"
+import AboutUs from "../../Components/AboutUs"
+import Container from '@mui/material/Container';
+
+export default function UserAboutUs(){
+
+  return(
+   <>
+    <ResponsiveAppBar/>
+    <img src="img/aboutus_banner.png" alt="logo" className="banner"/>
+    <br/>
+    <br/>
+    <br/>
+    <Container maxWidth="lg">
+      <AboutUs/>
+    </Container>
+    
+  
+      
+  </>
+  )
+}
