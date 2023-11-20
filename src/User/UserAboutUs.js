89243f66e09fa26af45a7faf7a87041abdf55429
Robert <amabarobert@gmail.com>
2023-11-20 16:48:15 +0800

    update aboutus

--- src/User/UserAboutUs.js
+++ src/User/UserAboutUs.js
@@ -1,23 +1,10 @@
 import React from "react"
 import ResponsiveAppBar from "../Components/header"
-<<<<<<< HEAD
-import EventCatBtn from "../Components/EventCatBtn"
-// import "../EventCategory/eventCategory.css"
-=======
 import "../User/UserEventCategory.css"
->>>>>>> e58984aa8f38f9f6d27c6eba44e59b270c0e51b2
 import "../Components/EventCatBtn.css"
 import AboutUs from "../Components/AboutUs"
 import Container from '@mui/material/Container';
 
-<<<<<<< HEAD
-export default function UserAboutUs(){
-
-  return(
-   <>
-    <ResponsiveAppBar/>
-    <img src="./img/aboutus_banner.png" alt="logo" className="banner"/>
-=======
 export default function PersonPics(){
 const Developers = [
   {
@@ -43,29 +30,22 @@ const Developers = [
    <>
     <ResponsiveAppBar/>
     <img src="img/aboutus_banner.png" alt="logo" className="banner"/>
->>>>>>> e58984aa8f38f9f6d27c6eba44e59b270c0e51b2
     <br/>
     <br/>
     <br/>
     <Container maxWidth="lg">
       <AboutUs/>
       <div>
-        {Developers.map((developer, index) => (
-                    <div
-                      key={index}
-                      style={{
-                        // flex: "0 0 25%",
-                        boxSizing: "border-box",
-                        padding: ".5rem",
-                      }}
-                    >
+         {/* {Developers.map((developer, index) => (
+                    <div>
                       <PersonPics
+                        
                         image={developer.image}
                         name={developer.name}
                         motto={developer.motto}
                       />
                     </div>
-                  ))}
+                  ))}  */}
         
       </div>
     </Container>
