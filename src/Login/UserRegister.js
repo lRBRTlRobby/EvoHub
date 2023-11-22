d7ac7298fa5caa4d452d59181ba94c900a863aaa
Robert <amabarobert@gmail.com>
2023-11-23 02:53:08 +0800

    register update

--- src/Login/UserRegister.js
+++ src/Login/UserRegister.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button, Grid, TextField } from "@mui/material";
 import './UserRegister.css'
-import { Link } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 import Container from '@mui/material/Container';
 import axios from 'axios';
 
 export default function EventRegister() {
+    const [regis, setRegis] = useState(false);
+    const navigate = useNavigate(); 
     const [userData, setUserData] = useState({
         fname: '',
         mname: '',
@@ -43,17 +45,16 @@ export default function EventRegister() {
     const handleSignUp = async () => {
         if (document.getElementById("fname").value != '' && document.getElementById("mname").value != '' && document.getElementById("lname").value != '' && document.getElementById("dept").value != '' && document.getElementById("email").value != '' && document.getElementById("pass").value != '') {
             if (document.getElementById("pass").value.length < 8) {
-                alert('Please enter a password.');
+                alert('Password must be at least 8 characters.');
                 return;
             }
             if (document.getElementById("pass").value.length >= 8) {
-                if (!(/[A-Z]/.test(document.getElementById("pass").value) && /[a-z]/.test(document.getElementById("pass").value))) {
-                    alert('Wrong input password.');
+                if (!(/[A-Z]/.test(document.getElementById("pass").value) && /[a-z]/.test(document.getElementById("pass").value) && /[\W_]/.test(document.getElementById("pass").value))) {
+                    alert('Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.');
                     return;
                 }
             }
-
-
+            setRegis(true);
             try {
                 const response = await axios.post('http://localhost:8080/User/insertUser', {
                     fname: document.getElementById("fname").value,
@@ -65,6 +66,7 @@ export default function EventRegister() {
                 });
                 console.log('Registration successful:', response.data);
                 // Redirect or show a success message as needed
+                
             } catch (error) {
                 console.error('Error during registration:', error);
                 // Handle registration failure, show error message, etc.
@@ -74,6 +76,14 @@ export default function EventRegister() {
             alert('Please fill all the fields');
         }
     };
+
+    useEffect(() => {
+        // Check if registration is successful
+        if (regis) {
+            // Redirect or show a success message as needed
+            navigate('/');
+        }
+    }, [regis]);
 
 
     return (
@@ -146,6 +156,9 @@ export default function EventRegister() {
 
 
                                 </div>
+                                {/* {regis ? <Link to="/"><Button sx={{ backgroundColor: "#800000", ":hover": { backgroundColor: "#800000" } }} variant="contained" onClick={handleSignUp}>Sign up</Button></Link> : 
+                                <Button sx={{ backgroundColor: "#800000", ":hover": { backgroundColor: "#800000" } }} variant="contained" onClick={handleSignUp}>Sign up</Button>
+                                } */}
                                 <Button sx={{ backgroundColor: "#800000", ":hover": { backgroundColor: "#800000" } }} variant="contained" onClick={handleSignUp}>Sign up</Button>
                             </div>
                         </Grid>
