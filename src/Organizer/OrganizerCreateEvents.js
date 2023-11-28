2368536b229f7a818d026caee7bfba019faf057b
Robert <amabarobert@gmail.com>
2023-11-28 15:55:25 +0800

    fix year 4

--- src/Organizer/OrganizerCreateEvents.js
+++ src/Organizer/OrganizerCreateEvents.js
@@ -486,7 +486,7 @@ export default function CreateEventForm() {
                     <option value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
-                    <option value="4<">4</option>
+                    <option value="4">4</option>
                     <option value="5">5</option>
                     <option value="None">None</option>  {/* Corrected the spelling here */}
                   </select>
