a47ed66474e95a6239b369d449bd19b009000284
IrishLeigh <irishleigh.sanjuan@cit.edu>
2023-11-30 00:27:10 +0800

    put texcxt ehre anything

--- src/User/UserEventCategoryCMBA.js
+++ src/User/UserEventCategoryCMBA.js
@@ -1,10 +1,12 @@
-import React, { useRef } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import ResponsiveAppBar from "../Components/header"
 import Container from '@mui/material/Container';
 import "./UserEventCategory"
 import ActionAreaCard from '../Components/eventCard';
 import { Button } from '@mui/material';
 import Footer from '../Components/footer';
+import axios from 'axios'
+import { Link } from 'react-router-dom';
 
 
 
@@ -12,6 +14,8 @@ export default function EventCategoryCMBA() {
     window.scroll(0, 0);
     const containerRef = useRef(null);
     const containerRef1 = useRef(null);
+    const [event, setEvents] = useState([]);
+    const currentDate = new Date();
 
     const scrollLeft = () => {
         if (containerRef.current) {
@@ -36,23 +40,6 @@ export default function EventCategoryCMBA() {
         }
     };
 
-    const events = [
-        { date: "Sep 04", title: "CIT-U Info Session 2022", image: "/img/irish.jpg", description: "Doggo and puppy" },
-        { date: "Sep 04", title: "TechXperience 2023", image: "/img/robert.png", description: "Doggo and puppy" },
-        { date: "Sep 04", title: "Summer Camp", image: "/img/kaye.jpg", description: "Doggo and puppy" },
-        { date: "Sep 04", title: "School Night", image: "/img/engineering.png", description: "Doggo and puppy" },
-        { date: "Sep 04", title: "Hello World", image: "/img/Joined.jpg", description: "Doggo and puppy" },
-        { date: "Sep 04", title: "Goodbye World", image: "/img/ccs.png", description: "Doggo and puppy" },
-        { date: "Sep 04", title: "CIT-U Info Session 2022", image: "/img/doggo.jpg", description: "Doggo and puppy" },
-        { date: "Sep 04", title: "TechXperience 2023", image: "/img/account.png", description: "Doggo and puppy" },
-        { date: "Sep 04", title: "Summer Camp", image: "/img/organreq.jpg", description: "Doggo and puppy" },
-        { date: "Sep 04", title: "School Night", image: "/img/engineering.png", description: "Doggo and puppy" },
-        { date: "Sep 04", title: "Hello World", image: "/img/Joined.jpg", description: "Doggo and puppy" },
-        { date: "Sep 04", title: "Goodbye World", image: "/img/ccs.png", description: "Doggo and puppy" }
-
-    ];
-
-
     return (
         <>
             <ResponsiveAppBar />
@@ -66,43 +53,68 @@ export default function EventCategoryCMBA() {
                 <div>
 
                     <div style={{ display: "flex", overflowX: "hidden", maxWidth: "100%" }} ref={containerRef}>
-                        {events.map((event, index) => (
-                            <ActionAreaCard
+                    {event.map((event, index) => (
+                        <div
+                            key={index}
+                            style={{
+                            boxSizing: "border-box",
+                            padding: ".5rem",
+                            }}
+                        >
+                            {/* Conditional rendering based on event date and department */}
+                            {new Date(event.date) >= currentDate && (event.department === "CEA" || event.department === "None") && (
+                            <Link to={`/UserEventPage/${event.eventid}`}>
+                                <ActionAreaCard
                                 key={index}
                                 date={event.date}
                                 title={event.title}
-                                image={event.image}
+                                image={"/uploads/" + event.image}
                                 description={event.description}
-                            />
+                                />
+                            </Link>
+                            )}
+                        </div>
                         ))}
                     </div>
                     <div style={{ display: "flex", justifyContent: "center" }}>
                         <Button onClick={scrollLeft}><img src="/img/leftbtn.png" alt="left" /></Button>
                         <Button onClick={scrollRight}><img src="/img/rightbtn.png" alt="left" /></Button>
                     </div>
-                </div>
+                    </div>
 
-                <div>
+                    <div>
                     <h2 style={{ fontFamily: "'DM Sans', sans-serif", marginTop: "5rem",fontSize:'30px' }}>Past Events</h2>
 
                     <div style={{ marginBottom: "5rem" }}>
                         <div style={{ display: "flex", flexWrap: "wrap", overflowX: "auto" , alignItemsL:"center", justifyContent: "center"}}>
-                            {events.map((event, index) => (
-                                <div key={index} style={{  boxSizing: "border-box" }}>
-                                    <ActionAreaCard
-                                        date={event.date}
-                                        title={event.title}
-                                        image={event.image}
-                                        description={event.description}
-                                    />
-                                </div>
-                            ))}
+                        {event.map((event, index) => (
+                        <div
+                            key={index}
+                            style={{
+                            boxSizing: "border-box",
+                            padding: ".5rem",
+                            }}
+                        >
+                            {/* Conditional rendering based on event date */}
+                            {new Date(event.date) <= currentDate && event.department === "CEA" && (
+                            <Link to={`/UserEventPage/${event.eventid}`}>
+                                <ActionAreaCard
+                                key={index}
+                                date={event.date}
+                                title={event.title}
+                                image={"/uploads/" + event.image}
+                                description={event.description}
+                                />
+                            </Link>
+                            )}
+                        </div>
+                        ))}
                         </div>
                     </div>
-                </div>
-            </Container>
-        
-        <Footer/>
-        </>
+                    </div>
+                    </Container>
+
+                    <Footer/>
+                    </>
     )
 }
